1d86c0c996e82a1a68792577726d1a9c1290100a
ilap42 <ilap435@gmail.com>
2025-04-14 20:21:02 -0600

    Set email config for development and deploy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -233,3 +233,14 @@ CACHES = {
 }
 
 CHANNELS_ALLOWED_ORIGINS = "http://localhost:3000"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+if not DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = env("EMAIL_HOST")
+    EMAIL_PORT = env("EMAIL_PORT")
+    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = env("EMAIL_USE_TLS") == "True"
+    DEFAULT_FROM_EMAIL = "ilap430 <no-reply@com> "
